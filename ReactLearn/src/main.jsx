829b05ffaf62831c450c8141ac62d8817bc2ee2c
ytrets3 <yuriy.tretsyak@gmail.com>
2026-03-02 23:09:02 +0300

    HW2. Init Vite React project
    Added vite empty project for react app.

---
+++ ReactLearn/src/main.jsx
@@ -0,0 +1,8 @@
+import { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './components/app.jsx';
+
+const root = document.getElementById('root');
+const reactRoot = createRoot(root);
+
+reactRoot.render("Hello World");
