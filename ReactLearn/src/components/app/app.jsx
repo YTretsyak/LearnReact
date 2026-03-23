cdb87f77cfb9dbf6508b837c013709245b25ce00
ytrets3 <yuriy.tretsyak@gmail.com>
2026-03-23 15:13:17 +0300

    HW5. Use ref example practice

--- ReactLearn/src/components/app/app.jsx
+++ ReactLearn/src/components/app/app.jsx
@@ -5,11 +5,33 @@ import { useState } from "react";
 import { ScrollProgress } from "../scrollProgress/scrollProgress";
 import styles from "./app.module.css";
 import { RestaurantButton } from "../restaurantButton/button";
+// import { useRef } from "react";
 
 export const App = () => {
   const [activeRestaurant, setActiveRestaurant] = useState(
     restaurants.length > 0 ? restaurants[0] : null,
   );
+  const buttonsBodyRef = useRef(null);
+  useEffect(() => {
+    if (buttonsBodyRef.current) {
+      console.log(buttonsBodyRef.current);
+      console.log(buttonsBodyRef.current.clientHeight);
+    }
+  }, []);
+
+  // const [timeInSecond, setTimeInSecond] = useState(0);
+
+  // var intervalElement = useRef(null);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setTimeInSecond((prev) => prev + 1);
+  //     intervalElement.current.innerText = timeInSecond;
+  //   }, 1000);
+
+  //   return () => clearInterval(interval);
+  // }, [timeInSecond]);
+
   return (
     <Layout>
       <ScrollProgress />
