1d67d918c21f6ee3aca4d53c2bc3cfb2d7e7a17a
ytrets3 <yuriy.tretsyak@gmail.com>
2026-03-23 17:52:57 +0300

    HW5. Add CUstom redux for user context

---
+++ ReactLearn/src/components/store/index.js
@@ -0,0 +1,26 @@
+import { createStore } from "../custom-redux/store";
+
+const defaultState = { user: {name: null}, loggedIn: false };
+
+const rootReducer = (state, action) => {
+  switch (action?.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        user: { name: "John Doe" },
+        loggedIn: true,
+      };
+
+    case "LOGOUT":
+      return {
+        ...state,
+        user: null,
+        loggedIn: false,
+      };
+    default:
+      return state;
+  }
+};
+
+const createdStoreFunc = createStore();
+export const store = createdStoreFunc(rootReducer, defaultState);
